eee4b6e50118d56e37100393a57539a981121579
Jacobus Lock <jacobus.lock@fox-robotics.com>
2022-09-12 17:59:02 +0100

    Added cmp to the init.lua to enable it

--- nvim/lua/nvim_config/cmp.lua
+++ nvim/lua/nvim_config/cmp.lua
@@ -29,17 +29,17 @@ cmp.setup({
     ['<Down>'] = cmp.mapping.select_next_item(select_opts),
     ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
     ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-    '<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-    
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        local col = vim.fn.col('.') - 1
+      
+        if cmp.visible() then
+          cmp.select_next_item(select_opts)
+        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+          fallback()
+        else
+          cmp.complete()
+        end
+      end, {'i', 's'}),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item(select_opts)
