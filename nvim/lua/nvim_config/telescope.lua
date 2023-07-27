eecc98317296ee5ca7c452fc61ae79d5744bf8ff
Jaycee Lock <jaycee.lock@gmail.com>
2023-07-27 18:44:40 +0100

    Removed frecency, causes more problems than its worth

--- nvim/lua/nvim_config/telescope.lua
+++ nvim/lua/nvim_config/telescope.lua
@@ -12,8 +12,6 @@ require('telescope').setup {
 
 -- Enable fzf fuzzy search sorter
 require('telescope').load_extension('fzf')
--- Enable frecency search
-require('telescope').load_extension('frecency')
 
 vim.api.nvim_set_keymap('n', '<leader>fc', '<cmd>lua require("telescope").extensions.frecency.frecency()<cr>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', { noremap = true, silent = true})
