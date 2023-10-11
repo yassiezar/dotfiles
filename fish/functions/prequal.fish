8f3b6a4d78bf8d63d0d83dab9e3f7021bdfabe45
Jaycee Lock <jlock@mathworks.com>
2023-10-11 08:43:02 +0100

    Added autosar_realtime_tests

--- fish/functions/prequal.fish
+++ fish/functions/prequal.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function prequal --wraps='sbprequal -copy-to-sbs -opened -cfg autosar -levels check_coverage_test -session-priority 2' --description 'alias prequal=sbprequal -copy-to-sbs -opened -cfg autosar -levels check_coverage_test -session-priority 2'
-  sbprequal -copy-to-sbs -opened -cfg autosar -levels check_coverage_test -session-priority 2 $argv;
+function prequal --wraps='sbprequal -copy-to-sbs -opened -cfg autosar -levels check_coverage_test -session-priority 2' --description 'alias prequal=sbprequal -copy-to-sbs -opened -cfg autosar autosar_realtime_tests -levels check_coverage_test -session-priority 2'
+  sbprequal -copy-to-sbs -opened -cfg autosar autosar_realtime_tests -levels check_coverage_test -session-priority 2 $argv;
 end
