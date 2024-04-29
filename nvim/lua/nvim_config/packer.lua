9fe9423c35d66c27029e954a40699be10f0c19ad
Jaycee Lock <jaycee.lock@gmail.com>
2024-04-29 14:38:33 +0100

    Added packer boostrap for nvim

--- nvim/lua/nvim_config/packer.lua
+++ nvim/lua/nvim_config/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function()
     --- Package manager
     use("wbthomason/packer.nvim")
@@ -114,4 +127,10 @@ return require("packer").startup(function()
     }
 
     use { 'sourcegraph/sg.nvim', run = 'nvim -l build/init.lua' }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
 end)
