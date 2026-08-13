9a398614ce71a689998df78ee887e42d270406ac
Jaycee Lock <jaycee@fruitcast.co.uk>
2026-08-13 09:21:30 +0100

    added omo-slim to opencode config

---
+++ fish/conf.d/opencode-background-subagents.fish
@@ -0,0 +1,4 @@
+# >>> oh-my-opencode-slim background subagents >>>
+set -gx OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS true
+set -gx OPENCODE_ENABLE_EXA 1
+# <<< oh-my-opencode-slim background subagents <<<
